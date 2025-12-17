779a412ce845ccc37a26241529e2ef3175c5c4e1
mohammad-al-asad <maasad11914@gmail.com>
2025-12-17 11:19:55 +0600

    fixed bug

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,32 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { SplashScreen, Stack } from "expo-router";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { Provider } from "react-native-paper";
 import AuthContextProvider from "./contexts/AuthContextProvider";
 
 export default function RootLayout() {
-  SplashScreen.preventAutoHideAsync();
-  const isOld = useRef("");
-  const isAuth = useRef("");
+  const [initialRoute, setInitialRoute] = useState<string | null>(null);
+
   useEffect(() => {
     (async function () {
-      isOld.current = (await AsyncStorage.getItem("isOld")) || "";
-      isAuth.current = (await AsyncStorage.getItem("token")) || "";
+      const isOld = (await AsyncStorage.getItem("isOld")) || "";
+      const token = (await AsyncStorage.getItem("token")) || "";
+      setInitialRoute(isOld ? (token ? "(tab)" : "auth") : "onboarding");
       await SplashScreen.hideAsync();
     })();
   }, []);
+
+  if (!initialRoute) {
+    return null;
+  }
+console.log(initialRoute);
 
   return (
     <AuthContextProvider>
       <Provider>
         <Stack
-          screenOptions={{
-            headerShown: false,
-          }}
-          initialRouteName={isOld ? (isAuth ? "(tab)" : "auth") : "onboarding"}
+          screenOptions={{ headerShown: false }}
+          initialRouteName={initialRoute}
         >
           <Stack.Screen name="(tab)" />
           <Stack.Screen name="onboarding" />
